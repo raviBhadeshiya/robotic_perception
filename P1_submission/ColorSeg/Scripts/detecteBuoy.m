7fc5a9c977e7deeecde15a9a19ec299d3d18efb8
Ravi Bhadeshiya <ravib@umd.edu>
2017-03-05 00:50:56 -0500

    Initial commit

--- P1_submission/ColorSeg/Scripts/detecteBuoy.m
+++ P1_submission/ColorSeg/Scripts/detecteBuoy.m
@@ -13,6 +13,8 @@ blue=I(:,:,3);
 for i=1:size(I,1)
     for j=1:size(I,2)
         x=[red(i,j) green(i,j) blue(i,j)]';
+%         x=[red(i,j) 0 0]';
+% % %         x=[red(i,j)+green(i,j)]';
 %   mask(i,j)=exp(-0.5*(double(x)-mu)'*inv(sig)*(double(x)-mu))/(((2*pi)^3/2)*sqrt(det(sig)));
         p=exp(-0.5*(double(x)-mu)'*inv(sig)*(double(x)-mu))/(((2*pi)^3/2)*sqrt(det(sig)));
         if p >= thre
@@ -27,11 +29,12 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute the location of the ball center
 mask_biggest = false(size(mask));
+% mask_biggest=imclose(mask_biggest,strel('disk',15));
 CC = bwconncomp(mask);
 numPixels = cellfun(@numel,CC.PixelIdxList);
-[biggest,idx] = max(numPixels);
+[biggest,idx] = min(numPixels);
 mask_biggest(CC.PixelIdxList{idx}) = true;
 S = regionprops(CC,'Centroid');
 loc = S(idx).Centroid;
-segI = imfill(mask_biggest);
+segI = imfill(mask_biggest,(round(loc)));
 end
--- P1_submission/ColorSeg/Scripts/example_test.m
+++ P1_submission/ColorSeg/Scripts/example_test.m
@@ -1,7 +1,7 @@
 close all;clear all;clc;
 load Parameter.mat;
 cd ..;
-folder = @(i) fullfile(sprintf('Images/TrainingSet/Frames/%03d.jpg',i));
+folder = @(i) fullfile(sprintf('Images/TestSet/Frames/%03d.jpg',i));
 to=20;
 image=cell(to,1);
 for k=1:to
@@ -9,11 +9,11 @@ for k=1:to
 end
 cd Scripts;
 for k=1:to
-    figure,imshow(image{k});
-    [segI, loc] = detecteBuoy(image{k},yellow_mu,yellow_sigma,1e-6);
-% % %     figure, imshow(segI); 
-% %     hold on; 
+    figure(1),imshow(image{k});
+    [segI, loc] = detecteBuoy(imgaussfilt(image{k}),red_mu,red_sigma,1e-7);
+%     figure, imshow(segI); 
+    hold on; 
     plot(loc(1), loc(2), '+r','MarkerSize',10); 
-    disp('Press any key to continue. (Ctrl+c to exit)')
+%     disp('Press any key to continue. (Ctrl+c to exit)')
     pause(1/10);
 end
