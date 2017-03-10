925933e08ed6906270e5390e35b929519f810a2f
Ravi Bhadeshiya <rnbhadeshiya@gmail.com>
2017-03-10 16:25:41 -0500

    Final Submission Commit

--- P1_submission/AR/Scripts/runMe.m
+++ P1_submission/AR/Scripts/runMe.m
@@ -1,5 +1,4 @@
 clear all;close all;clc;warning('off','all');
-
-frameProcess('Tag0',1,540);
-% frameProcess('Tag1',1,2);
-% frameProcess('Tag2',1,2);
+frameProcess('Tag0',1,121);
+frameProcess('Tag1',259,381);
+frameProcess('Tag2',34,154);
